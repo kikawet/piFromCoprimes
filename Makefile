0f6ac6de4c9a64b3292c519e1317c0b639eb2c59
kikawet <lopezortegafrancisco1@gmail.com>
2022-10-16 21:01:52 +0200

    refactor delete + format code

--- Makefile
+++ Makefile
@@ -1,10 +1,15 @@
 all:
-	mkdir build
+	mkdir -p build
 	g++ -Wall -O0 -pthread -std=c++17 -o build/main main.cpp
 
 clean:
 	rm -rf build
+	rm -rf release
 	rm -f *.gcda
 	rm -f *.gcno
 	rm -f *.gcov
 	rm -rf bw-output
+
+release:
+	mkdir -p release
+	g++ -Wall -O3 -pthread -std=c++17 -o release/main main.cpp
--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <random>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <boost/multiprecision/cpp_dec_float.hpp>
-#include <cmath>
 #include <fstream>
 #include <vector>
 #include <utility>
@@ -16,78 +15,79 @@ using namespace std::chrono_literals;
 
 std::vector<std::pair<cpp_int, cpp_int>> calculations;
 
-void calculateValues(int index, cpp_int limit, cpp_int logPeriod);
+void calculateValues(int index, const cpp_int &limit, const cpp_int &logPeriod);
 
 int main() {
-  const int numOfThreads = 5;
-  cpp_int limit = (cpp_int(1) << 128)
-      , updatePeriod = (cpp_int(1) << 16);
-  std::vector<std::future<void>> threads;
-  
-  for(int i = 0; i < numOfThreads; ++i){    
-    threads.push_back(std::move(std::async(std::launch::async,[&](){
-      calculateValues(i, limit, updatePeriod);
-    })));
-        
-    calculations.push_back(std::make_pair(0,0));
-  }
+    const int numOfThreads = 5;
+    cpp_int limit = (cpp_int(1) << 128)
+    , updatePeriod = (cpp_int(1) << 16);
+    std::vector<std::future<void>> threads;
 
-  std::cout << "Pi = " << std::setprecision(20) << std::endl;
-  cpp_int numOfCoprimes, totalNumbers;
-  
-  while(threads.size()){
-    for(int i = threads.size()-1; i >= 0; --i){
-      if(threads[i].wait_for(1s) == std::future_status::ready){
-        threads.erase(threads.begin() + i);
-      }
+    for (int i = 0; i < numOfThreads; ++i) {
+        threads.push_back(std::async(std::launch::async, [&]() {
+            calculateValues(i, limit, updatePeriod);
+        }));
+
+        calculations.emplace_back(0, 0);
     }
 
-    std::this_thread::sleep_for(2s);
+    std::cout << "Pi = " << std::setprecision(20) << std::endl;
+    cpp_int numOfCoprimes, totalNumbers;
+
+    while (!threads.empty()) {
+        const auto firstRemove = std::remove_if(threads.begin(), threads.end(), [](const auto &thread) {
+            return thread.wait_for(1s) == std::future_status::ready;
+        });
+
+        threads.erase(firstRemove, threads.end());
+
+        std::this_thread::sleep_for(2s);
+        numOfCoprimes = 0, totalNumbers = 0;
+        for (const auto &cal: calculations) {
+            numOfCoprimes += cal.first;
+            totalNumbers += cal.second;
+        }
+
+        std::cout << sqrt(
+                6 / (
+                        (cpp_dec_float_100) numOfCoprimes /
+                        (cpp_dec_float_100) totalNumbers
+                )
+        ) << std::endl;
+    }
+
+
+    std::cout << "All threads are over" << std::endl;
+
     numOfCoprimes = 0, totalNumbers = 0;
-    for(const auto& cal: calculations){
-      numOfCoprimes += cal.first;
-      totalNumbers += cal.second;
+    for (const auto &cal: calculations) {
+        numOfCoprimes += cal.first;
+        totalNumbers += cal.second;
     }
 
-    std::cout << sqrt(
-        6/(
-          (cpp_dec_float_100)numOfCoprimes/
-          (cpp_dec_float_100)totalNumbers
-        )
-      ) << std::endl;
-  }
-
-  std::cout << "All threads are over" << std::endl;
-
-  numOfCoprimes = 0, totalNumbers = 0;
-  for(const auto& cal: calculations){
-    numOfCoprimes += cal.first;
-    totalNumbers += cal.second;
-  }
-
-  std::ofstream file("PI.txt");
+    std::ofstream file("PI.txt");
 
 
-  file << "PI = " << std::setprecision(100) << sqrt(
-        6/(
-          (cpp_dec_float_100)numOfCoprimes/
-          (cpp_dec_float_100)totalNumbers
-        )
-      ) << std::endl;
+    file << "PI = " << std::setprecision(100) << sqrt(
+            6 / (
+                    (cpp_dec_float_100) numOfCoprimes /
+                    (cpp_dec_float_100) totalNumbers
+            )
+    ) << std::endl;
 
-file.close();
-std::cout << "Finished storing result into PI.txt" << std::endl;
+    file.close();
+    std::cout << "Finished storing result into PI.txt" << std::endl;
 }
 
-void calculateValues(int index, cpp_int limit, cpp_int logPeriod){
-  std::random_device rd;
-  cpp_int numOfCoprimes = 0, iteration = 0;
-  
-  while(++iteration < limit){
-    if(std::gcd(rd(),rd()) == 1)
-      ++numOfCoprimes;    
+void calculateValues(int index, const cpp_int &limit, const cpp_int &logPeriod) {
+    std::random_device rd;
+    cpp_int numOfCoprimes = 0, iteration = 0;
 
-    if(iteration%logPeriod == 0)
-      calculations[index] = std::make_pair(numOfCoprimes,iteration);
-  }
+    while (++iteration < limit) {
+        if (std::gcd(rd(), rd()) == 1)
+            ++numOfCoprimes;
+
+        if (iteration % logPeriod == 0)
+            calculations[index] = std::make_pair(numOfCoprimes, iteration);
+    }
 }
